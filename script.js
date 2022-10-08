5a722e6d43362dae0e50b6e7bb02f93ecfd10503
Caleb D. Sim <103973059+CodeLamp168@users.noreply.github.com>
2022-10-08 09:29:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const email = document.getElementById('email')
+const form = document.getElementById('email-form')
+
+const erNotif = document.getElementById('error')
+
+
+
+
+form.addEventListener('submit', (e) => {
+
+    let emailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    
+
+    if(email.value.match(emailformat))
+    {
+        email.classList.add('input-correct')
+        error.classList.remove('error-notif')
+        
+    }
+
+    else 
+    {
+        email.classList.remove('input-correct')
+        email.classList.add('input-error')
+        error.classList.add('error-notif')
+    }
+
+    e.preventDefault()
+
+})
